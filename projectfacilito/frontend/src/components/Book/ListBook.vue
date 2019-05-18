afbccf2c8745a02079e6b2775b5893c85bcf40ed
tekmickey <tekmickey0118@gmail.com>
2019-05-18 15:33:43 -0500

    first commit

---
+++ projectfacilito/frontend/src/components/Book/ListBook.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col text-left">
+                <div>
+                    <h2>Listado de libros</h2>
+                    <b-button size="sm" :to="{name: 'NewBook'}" variant="primary">Nuevo Libro</b-button>
+                </div>
+                <br>
+                <div class="col-md-12">
+                    <b-table stripped hover :items="books" :fields="fields" >
+                        <template slot="action" slot-scope="data">
+                            <b-button size="sm" variant="primary" :to="{name:'EditBook', params:{bookId:data.item.id}}" >Editar</b-button>
+                            <b-button size="sm" variant="danger" :to="{name:'DeleteBook', params:{bookId:data.item.id}}">Eliminar</b-button>
+                        </template>
+                    </b-table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data () {
+        return{
+            fields: [
+                {key: 'title', label: 'Título'},
+                {key: 'description', label: 'Descripción'},
+                {key: 'action', label: 'Acción'}
+            ],
+            books: []
+        }
+    },
+    methods: {
+        getBooks () {
+            const path = 'http://127.0.0.1:8000/api/v1.0/books/';
+            axios.get(path).then((response) => {
+                this.books = response.data;
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
+    },
+    created () {
+        this.getBooks();
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ projectfacilito/frontend/src/components/Book/NewBook.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col text-left">
+                <h2>Nuevo Libro</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <form @submit="onSubmit">
+                            <div class="form-group row">
+                                <label for="title" class="col-sm-2 col-form-label">Crear</label>
+                                <div class="col-sm-6">
+                                    <input type="text" placeholder="Título" name="title" class="form-control" v-model.trim="form.title">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="description" class="col-sm-2 col-form-label">Descripción</label>
+                                <div class="col-sm-6">
+                                    <textarea name="description" class="form-control" placeholder="Descripción" rows="3" v-model.trim="form.description"></textarea>
+                                </div>
+                            </div>
+                            <div class="rows">
+                                <div class="col text-left">
+                                    <div>
+                                        <b-button type="submit" variant="primary">Crear</b-button>
+                                        <b-button type="submit" class="btn-large-space" :to="{name: 'ListBook'}">Cancelar</b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import swal from 'sweetalert';
+
+export default {
+    data () {
+        return {
+            form: {
+                title: '',
+                description: ''
+            }
+        }
+    },
+    methods: {
+        onSubmit(evt){
+            evt.preventDefault();
+            const path = `http://127.0.0.1:8000/api/v1.0/books/`;
+            axios.post(path, this.form).then((response) => {
+                this.form.title = response.data.title;
+                this.form.description = response.data.description;
+                swal("¡Libro creado exitosamente!", "", "Succes");
+            }).catch((error) => {
+                swal("El libro no ha sido creado", "", error);
+            });
+        },
+    },
+    created() {
+    },
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
